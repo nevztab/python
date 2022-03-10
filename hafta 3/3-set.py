3e4df24e25e4495aee69257a2c67dd05cb8e9dd3
nevztab <91660598+nevztab@users.noreply.github.com>
2022-03-10 16:23:35 +0300

    Update 3-set.py

--- hafta 3/3-set.py
+++ hafta 3/3-set.py
@@ -1,3 +1,38 @@
-arabalar={"bmw","mercedes","supra"} #set liste oluşturma
+#Sets
+
+61
+
+oyunlarYeni-('Valorant', 'Pubg', 'Cs Go', 'Mortal Combat'}
+
+62
+
+cikistarihleri-[1990, 2000, 2010]
+
+oyunlarYeni.add('Pes 13') #ilk indexe ekle
 
 
+
+
+
+oyunlarYeni.update (cikisTarihleri) #set i cikisTarihleri listesi ekleyerek güncelle
+
+
+
+
+
+#oyunlarYeni.remove(1995)
+
+#eleman siler. bulamazsa uyarı verir
+
+oyunlarYeni.discard (1995)
+
+oyunlarYeni.clear()
+
+#eleman siler. bulamazsa uyarı vermez
+
+#set listesinin içini boşalt
+
+
+
+
+print (oyunlarYeni)
