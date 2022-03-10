4ca5dbbca165c3c580f7b22b65dab5ad2ba0184c
nevztab <91660598+nevztab@users.noreply.github.com>
2022-03-10 16:36:12 +0300

    Update 4-dict.py

--- hafta 3/4-dict.py
+++ hafta 3/4-dict.py
@@ -1,1 +1,16 @@
 
+sozluk={
+
+'bmw': 'almanya', 'togg' : 'türkiye',
+
+'yil': 1000
+
+}
+
+#elemanlar erişim sonuc=sozluk.get('yil')
+
+sonuc=sozluk['bmw'] sonuc=sozluk.keys ()
+
+sonuc=sozluk.values() sonuc-sozluk.items()
+
+print (sonuc)
