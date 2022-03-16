a44bd1065a65bbb15e7be1226dad277003a4e29b
nevztab <91660598+nevztab@users.noreply.github.com>
2022-03-16 16:13:31 +0300

    Update 4-dict.py

--- hafta 3/4-dict.py
+++ hafta 3/4-dict.py
@@ -25,42 +25,26 @@ oyunlar={
 
 'tür': 'fps'
 
-}, 'Fifa': {
+}, 
+'Fifa': {
 
 'fiyat': 105,
 
 'tür': 'fps'
-
-print (oyunlar['Call of Duty']['fiyat'])
+}
+}
+# print (oyunlar['Call of Duty']['fiyat'])
 
 isimler={}
-
-ad-input('isim: ')
-
+ad=input('isim: ')
 okulNo=int (input('okulno: '))
-
 sinif=input('sınıf: ')
-
 isimler.update({
 
 ad:{
-
 'okul.no': okulNo,
-
 'sınıf':sinif
-
-5
-
-5
-
-)
-
-L
-
-2
-
 }
-
 })
 
 print (isimler)
