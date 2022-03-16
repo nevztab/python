660901f73aa04cc500006d427e3eff4d121d031a
nevztab <91660598+nevztab@users.noreply.github.com>
2022-03-16 16:38:14 +0300

    Update if-else.py

--- hafta 4/if-else.py
+++ hafta 4/if-else.py
@@ -1,1 +1,39 @@
-print
+a=20
+
+b=15
+
+# if a>b:
+
+#
+
+print(f {a) bayktür {b} den')
+
+#elif a<b:
+
+#
+
+print (f' {a} küçüktür {b} den')
+
+# else:
+
+#
+
+print (f {a} denktir (b} ye')
+
+if a>b:
+
+if a%2==0:
+
+print (f {a} büyüktür {b} den ve {a} çifttir')
+
+elif a%2==1:
+
+print (f {a} büyüktür {b} den ve {a} tektir')
+
+elif a<b:
+
+print (f'{a} küçüktür {b} den')
+
+else:
+
+print (f'{a} denktir {b} ye')
