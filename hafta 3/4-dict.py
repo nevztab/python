268b46d35576a4997621f3674e2b7c3f3b23d784
nevztab <91660598+nevztab@users.noreply.github.com>
2022-03-16 16:10:36 +0300

    Update 4-dict.py

--- hafta 3/4-dict.py
+++ hafta 3/4-dict.py
@@ -16,3 +16,51 @@ sozluk.clear()
 del sozluk['yil'] 
 
 print (sozluk)
+
+oyunlar={
+
+'Call of Duty':{
+
+'fiyat' :100,
+
+'tür': 'fps'
+
+}, 'Fifa': {
+
+'fiyat': 105,
+
+'tür': 'fps'
+
+print (oyunlar['Call of Duty']['fiyat'])
+
+isimler={}
+
+ad-input('isim: ')
+
+okulNo=int (input('okulno: '))
+
+sinif=input('sınıf: ')
+
+isimler.update({
+
+ad:{
+
+'okul.no': okulNo,
+
+'sınıf':sinif
+
+5
+
+5
+
+)
+
+L
+
+2
+
+}
+
+})
+
+print (isimler)
