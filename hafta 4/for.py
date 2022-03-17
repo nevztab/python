4afcce4d9570569521cdc010887943d12539bd77
nevztab <91660598+nevztab@users.noreply.github.com>
2022-03-17 16:18:17 +0300

    Update for.py

--- hafta 4/for.py
+++ hafta 4/for.py
@@ -1,18 +1,31 @@
 for i in range (10): #10 kez hello yaz
     print ('hello')
 
+
 for i in range(10): #0 dan 9 a i yi yaz
     print(i)
+
 
 listem= [1, 2,3,4]
 
 for eleman in listem: #listedeki elemanları 2 ile çarparak yaz
     print (eleman*2)
 
-for sayi in range (50,100): if sayi%2==0: print (f'çift {sayi}') elif sayi%2==1: print (f' tek {sayi}')
+
+for sayi in range (50,100): 
+
+    if sayi%2==0: 
+        print (f'çift {sayi}') 
+
+    elif sayi%2==1: 
+        print (f' tek {sayi}')
+        
+
 
 toplam=0
+sayilar=[13,15,45,4,50]
 
-sayilar=[13,15,45,4,50] for i in sayilar: toplam-toplam+i
+for i in sayilar: 
+    toplam-toplam+i
 
 print(toplam)
