15d1b6de792cf45b73461ee5d56711bde216e945
nevztab <91660598+nevztab@users.noreply.github.com>
2022-03-17 16:03:12 +0300

    Update for.py

--- hafta 4/for.py
+++ hafta 4/for.py
@@ -1,11 +1,10 @@
+for i in range (10): #10 kez hello yaz
+    print ('hello')
 
-for i in range (10): print ('hello')
-
-
-for i in range(10): 
- print(i)
+for i in range(10): #0 dan 9 a i yi yaz
+    print(i)
 
 listem= [1, 2,3,4]
 
-for eleman in listem:
- print (eleman*2)
+for eleman in listem: #listedeki elemanları 2 ile çarparak yaz
+    print (eleman*2)
