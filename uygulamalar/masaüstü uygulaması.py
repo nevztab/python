c9ae310872ce3c0b875ec431bd5dc3ae63a1423d
nevztab <91660598+nevztab@users.noreply.github.com>
2022-04-25 16:42:58 +0300

    Update masaüstü uygulaması.py

--- uygulamalar/masaüstü uygulaması.py
+++ uygulamalar/masaüstü uygulaması.py
@@ -1,4 +1,3 @@
-from cgitb import text
 from PyQt5 import QtWidgets
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -11,7 +10,7 @@ def uygulamam():
     pencere=QMainWindow()
     
 
-    pencere.setGeometry(100,400,600,500)
+    pencere.setGeometry(200,350,600,500)
     pencere.setWindowTitle("Hello")
     pencere.setToolTip("Ben geldim")
     pencere.setWindowIcon(QIcon('collie.png'))
@@ -25,7 +24,7 @@ def uygulamam():
     
 
     sonuc_label= QtWidgets.QLabel(pencere)
-    sonuc_label.move(100,150)
+    sonuc_label.move(225,50)
     sonuc_label.setText('sonuc')
 
     def kayıt():
@@ -36,8 +35,15 @@ def uygulamam():
     button_kayıt.setText('kayıt')
     button_kayıt.clicked.connect(kayıt)
 
-    
+    def dosya():
+        dosyam = open("dosyalar/kayıt.txt","w")
+        print(text_isim.text(),file=dosyam)
+        dosyam.close()
 
+    button_dosya= QtWidgets.QPushButton(pencere)
+    button_dosya.move(100,150)
+    button_dosya.setText('belgele')
+    button_dosya.clicked.connect(dosya)
 
 
     pencere.show()
